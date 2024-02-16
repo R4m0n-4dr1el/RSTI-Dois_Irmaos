8288573260a619dc92487837477d6f76a3e7729f
R4mon-4dr1el <ramonadrielsantos2@gmail.com>
2024-02-16 16:47:34 -0300

    aula de TI

---
+++ index.js
@@ -0,0 +1,65 @@
+/*var nome = prompt("Escreva qual é o seu nome?")
+var cor = prompt("Qual a sua cor favorita?")
+var frase = console.log("A cor favorita do " + nome + " é " + cor + ".")
+
+
+var frase = prompt("Digite uma frase");
+var frase2 = frase.toUpperCase();
+console.log(frase2);
+
+
+var substituto = prompt("Digite uma frase boa");
+var novaSubstituto = substituto.replaceAll("o", "i");
+console.log(novaSubstituto);
+console.log(novaSubstituto.length);
+
+const listaDeCompras = ["Abacate", "Banana", "Tomate"]
+const segundoItem = listaDeCompras[2]
+console.log(segundoItem)
+
+
+var raças = ["Dalmata", "Pastor-Alemão", "Labrador", "Husky", "Golden"];
+const frase = prompt("DIgite um número de 0 a 4");
+console.log(raças[frase]);
+
+console.log(raças[prompt("Digite de 0 a 4")])
+
+
+var array = ["a", "b", "c", "d", "e", "f", "g", "h"];
+//          [ 0,   1,   2,   3,   4,   5,   6,   7]
+array.splice(3, 1)
+console.log(array)
+
+
+var array = [1, 2, 3, 4, 5, 6]
+console.log(array.length)
+array.push(7)
+array.splice(3, 2);
+console.log(array.length)
+
+
+const altura = 2
+const largura = 3
+const area = altura*largura
+console.log(area)
+
+
+function calculoArea(altura, largura) {
+    const area = altura*largura
+    console.log(area)
+}
+calculoArea(2, 3)
+
+function imprimirOlaMundo(){
+    console.log("Ola mundo");
+}
+imprimirOlaMundo()
+*/
+
+function pessoa(nome) {
+    console.log("Olá ", nome);
+}
+pessoa("Lucas")
+pessoa("Pedro")
+pessoa("Ramon")
+ 
